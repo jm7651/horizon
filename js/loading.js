8cc379c36f06e56356b35fecfa09144e42529923
Jungmin Park <65692480+jm7651@users.noreply.github.com>
2024-08-15 22:14:55 +0900

    commit

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,14 @@
+// 페이지가 새로고침될 때마다 스크롤을 맨 위로 초기화
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
 
+// 로더 시작 함수
 function startLoader() {
   let counterElement = document.querySelector(".loading_counter");
   let currentValue = 0;
 
+  // 카운터 업데이트 함수
   function updateCounter() {
     if (currentValue === 100) {
       document.body.classList.remove("no-scroll"); // 로딩 완료 시 스크롤 허용
@@ -23,28 +26,41 @@ function startLoader() {
   updateCounter();
 }
 
-document.body.classList.add("no-scroll"); // 로딩 시작 시 스크롤 금지
+// 로딩 시작 시 스크롤 금지
+document.body.classList.add("no-scroll");
 
+// 로더 시작
 startLoader();
 
+// Lottie 애니메이션 설정
 var animation = lottie.loadAnimation({
-  container: document.getElementById("lottie"), // the dom element that will contain the animation
+  container: document.getElementById("lottie"), // 애니메이션을 담을 DOM 요소
   renderer: "svg",
   loop: false,
-  autoplay: true,
-  path: "images/horizon.json", // the path to the animation json
+  autoplay: false, // 자동 재생 끔
+  path: "images/horizon.json", // 애니메이션 JSON 경로
   rendererSettings: {
     preserveAspectRatio: "xMidYMid meet",
   },
 });
 
+// Lottie 애니메이션이 로드 완료되면 재생
+animation.addEventListener("DOMLoaded", function () {
+  animation.play();
+});
+
+// 애니메이션이 완료된 후 실행되는 코드
 animation.addEventListener("complete", function () {
   animation.goToAndStop(animation.totalFrames - 1, true); // 마지막 프레임 유지
+
+  // Lottie 애니메이션이 끝난 후 1.5초 대기 후 GSAP 애니메이션 실행
   setTimeout(function () {
+    // 로딩 카운터 숨기기
     gsap.to(".loading_counter", {
       opacity: 0,
     });
 
+    // 로딩 블록 축소
     gsap.to(".loading_block", {
       height: "0%",
       duration: 0.8,
@@ -55,14 +71,15 @@ animation.addEventListener("complete", function () {
       onComplete: function () {
         var mainVideo = document.getElementById("video_main");
         console.log(mainVideo);
-        mainVideo.play();
+        mainVideo.play(); // 비디오 재생 시작
       },
     });
 
+    // Lottie 컨테이너 숨기기
     gsap.to(".lottie-container", {
-      opacity: "0",
+      opacity: 0,
       duration: 0.8,
       ease: "power4.inOut",
     });
-  }, 1500); // 1.5초 후에 애니메이션 제거
+  }, 0);
 });
